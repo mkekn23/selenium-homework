8a3755484d9e189734ee305cecc2e7d985e33132
mkekn23 <mkekn23@freeuni.edu.ge>
2026-07-23 14:51:39 +0400

    დავალება - Selenium Getting Started

--- src/test/java/ge/tbc/testautomation/NavigationTest.java
+++ src/test/java/ge/tbc/testautomation/NavigationTest.java
@@ -1,16 +1,18 @@
 package ge.tbc.testautomation;
-
 import ge.tbc.testautomation.data.Constants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Map;
 
 public class NavigationTest {
@@ -28,14 +30,17 @@ public class NavigationTest {
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get(Constants.DRAG_AND_DROP_URL);
+        driver.get(Constants.ULTIMATE_QA_URL);
     }
 
 
     @Test(priority = 1, description = "check if services link works and also goes back")
     public void goToServicesAndBack(){
-        WebElement servicesLink = driver.findElement(By.xpath("//header//a[text()='Services']"));
+        WebElement servicesLink = driver.findElement(By.id("menu-item-218392"));
         servicesLink.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("consulting"));
+
         Assert.assertEquals(driver.getCurrentUrl(), Constants.CONSULTING_URL);
 
         driver.navigate().back();
